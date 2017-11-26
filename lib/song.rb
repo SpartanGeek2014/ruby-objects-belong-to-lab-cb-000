6437acf835f0d5090aba382311bd4bce94ebaf54
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-26 00:35:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,4 @@
 class Song
   attr_accessor :title, :artist
 
-  def initialize(title, artist)
-    @title = title
-    @artist = artist
-  end
-
-  def new_song(self.new)
-    new_song = self.new
-    new_song
-  end
 end
